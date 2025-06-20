f1688ba542cdde801a83f4b15ffb0f87e9a9a849
Alexander Suter <alexander.suter@axonivy.com>
2025-06-20 09:41:42 +0200

    Setup project

---
+++ src/test/java/com/axonivy/ivy/engine/cli/DummyTest.java
@@ -0,0 +1,13 @@
+package com.axonivy.ivy.engine.cli;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class DummyTest {
+
+  @Test
+  void test() {
+    assertThat(true).isTrue();
+  }
+}
